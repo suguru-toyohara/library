25e6e50f4da622ee663aceb7669e9b05628f0dea
suguru-toyohara <suguru_toyohara@aceof.technology>
2024-11-23 15:32:45 +0900

    initをformat

---
+++ Makefile
@@ -0,0 +1,3 @@
+.PHONY: check
+check:
+	@npx @biomejs/biome check --write ./src
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Page() {
-	return (
-		<div>
-			<p>hello world</p>
-		</div>
-	);
+  return (
+    <div>
+      <p>hello world</p>
+      <p className={'text-green-500'}>hello world</p>
+    </div>
+  );
 }
