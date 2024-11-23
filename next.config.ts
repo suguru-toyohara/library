237bdd6f506106402fae37d09016f600c534ab49
suguru-toyohara <suguru_toyohara@aceof.technology>
2024-11-23 16:36:17 +0900

    pnpm導入+MDX導入

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ["js", "jsx", "ts", "tsx", 'md', "mdx"],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // Add Markdown plugins here, as desired
+})
+
+export default withMDX(nextConfig);
